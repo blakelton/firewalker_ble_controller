20a3c9ee3107912f1466303f6c9fdf2ec147e519
Blake Azuela <blake@azuela.info>
2025-01-20 14:15:19 -0500

    Added code to main.cpp that does what I wanted to do: 1. Run Neopixels (addressible LEDs) 2. Broadcast a bluetooth connection
    Under PlatformIO project builds but board doesn't respond on upload. I want to keep the code.

--- platform/src/main.cpp
+++ platform/src/main.cpp
@@ -1,67 +1,251 @@
-#include <Arduino.h>
+/****************************************************************************
+ * This example is based on StrandtestBLE example and adapts it to use 
+ * the new ArduinoBLE library.
+ * 
+ * https://github.com/arduino-libraries/ArduinoBLE
+ * 
+ * Supported boards:
+ *  Arduino MKR WiFi 1010, Arduino Uno WiFi Rev2 board, Arduino Nano 33 IoT,
+    Arduino Nano 33 BLE, or Arduino Nano 33 BLE Sense board.
+ * 
+ * You can use a generic BLE central app, like LightBlue (iOS and Android) or
+ * nRF Connect (Android), to interact with the services and characteristics
+ * created in this sketch.
+ * 
+ * This example code is in the public domain.
+ * 
+ */
+#include <Adafruit_NeoPixel.h>
 
-// By default, on Seeed XIAO BLE (Sense) boards, the onboard APA102 LED is on:
-//   - DATA  = pin 13
-//   - CLOCK = pin 14
-#define DOTSTAR_DATA_PIN  13
-#define DOTSTAR_CLOCK_PIN 14
+// What PIN are we using the neopixels on?
+#define LED_PIN D0
 
-// Send one byte out via bit-bang on the DotStar pins
-void sendByte(uint8_t b) {
-  for (uint8_t i = 0; i < 8; i++) {
-    digitalWrite(DOTSTAR_DATA_PIN, (b & 0x80) ? HIGH : LOW);
-    digitalWrite(DOTSTAR_CLOCK_PIN, HIGH);
-    b <<= 1;
-    digitalWrite(DOTSTAR_CLOCK_PIN, LOW);
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT 24
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
+unsigned long pixelPrevious = 0;        // Previous Pixel Millis
+unsigned long patternPrevious = 0;      // Previous Pattern Millis
+int           patternCurrent = 0;       // Current Pattern Number
+int           patternInterval = 5000;   // Pattern Interval (ms)
+bool          patternComplete = false;
+
+int           pixelInterval = 50;       // Pixel Interval (ms)
+int           pixelQueue = 0;           // Pattern Pixel Queue
+int           pixelCycle = 0;           // Pattern Pixel Cycle
+uint16_t      pixelNumber = LED_COUNT;  // Total Number of Pixels
+
+// NEOPIXEL BEST PRACTICES for most reliable operation:
+// - Add 1000 uF CAPACITOR between NeoPixel strip's + and - connections.
+// - MINIMIZE WIRING LENGTH between microcontroller board and first pixel.
+// - NeoPixel strip's DATA-IN should pass through a 300-500 OHM RESISTOR.
+// - AVOID connecting NeoPixels on a LIVE CIRCUIT. If you must, ALWAYS
+//   connect GROUND (-) first, then +, then data.
+// - When using a 3.3V microcontroller with a 5V-powered NeoPixel strip,
+//   a LOGIC-LEVEL CONVERTER on the data line is STRONGLY RECOMMENDED.
+// (Skipping these may work OK on your workbench but can fail in the field)
+
+// Some functions of our own for creating animated effects -----------------
+
+// Fill strip pixels one after another with a color. Strip is NOT cleared
+// first; anything there will be covered pixel by pixel. Pass in color
+// (as a single 'packed' 32-bit value, which you can get by calling
+// strip.Color(red, green, blue) as shown in the loop() function above),
+// and a delay time (in milliseconds) between pixels.
+void colorWipe(uint32_t color, int wait) {
+  static uint16_t current_pixel = 0;
+  pixelInterval = wait;                        //  Update delay time
+  strip.setPixelColor(current_pixel++, color); //  Set pixel's color (in RAM)
+  strip.show();                                //  Update strip to match
+  if(current_pixel >= pixelNumber) {           //  Loop the pattern from the first LED
+    current_pixel = 0;
+    patternComplete = true;
   }
 }
 
-// Send color (R,G,B) to the single onboard DotStar
-// APA102 typically transmits in the order: StartFrame -> (Brightness),(B),(G),(R) -> EndFrame
-void showColor(uint8_t r, uint8_t g, uint8_t b) {
-  // Start frame: 4 bytes of 0x00
-  sendByte(0x00);
-  sendByte(0x00);
-  sendByte(0x00);
-  sendByte(0x00);
+// Theater-marquee-style chasing lights. Pass in a color (32-bit value,
+// a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
+// between frames.
+void theaterChase(uint32_t color, int wait) {
+  static uint32_t loop_count = 0;
+  static uint16_t current_pixel = 0;
 
-  // LED frame: 1st byte is global brightness (0xFF = max).
-  sendByte(0xFF);
-  // Next 3 bytes are B, G, R
-  sendByte(b);
-  sendByte(g);
-  sendByte(r);
+  pixelInterval = wait;                   //  Update delay time
 
-  // End frame: for one LED, 4 bytes of 0xFF is sufficient
-  sendByte(0xFF);
-  sendByte(0xFF);
-  sendByte(0xFF);
-  sendByte(0xFF);
+  strip.clear();
+
+  for(int c=current_pixel; c < pixelNumber; c += 3) {
+    strip.setPixelColor(c, color);
+  }
+  strip.show();
+
+  current_pixel++;
+  if (current_pixel >= 3) {
+    current_pixel = 0;
+    loop_count++;
+  }
+
+  if (loop_count >= 10) {
+    current_pixel = 0;
+    loop_count = 0;
+    patternComplete = true;
+  }
 }
 
-void setup() {
-  // Configure pins for output
-  pinMode(DOTSTAR_DATA_PIN, OUTPUT);
-  pinMode(DOTSTAR_CLOCK_PIN, OUTPUT);
-
-  // Turn off the LED initially
-  showColor(0, 0, 0);
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
-void loop() {
-  // Red for 1s
-  showColor(255, 0, 0);
-  delay(1000);
+// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+void rainbow(uint8_t wait) {
+  if(pixelInterval != wait)
+    pixelInterval = wait;                   
+  for(uint16_t i=0; i < pixelNumber; i++) {
+    strip.setPixelColor(i, Wheel((i + pixelCycle) & 255)); //  Update delay time  
+  }
+  strip.show();                             //  Update strip to match
+  pixelCycle++;                             //  Advance current cycle
+  if(pixelCycle >= 256)
+    pixelCycle = 0;                         //  Loop the cycle back to the begining
+}
 
-  // Green for 1s
-  showColor(0, 255, 0);
-  delay(1000);
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  if(pixelInterval != wait)
+    pixelInterval = wait;                   //  Update delay time  
+  for(int i=0; i < pixelNumber; i+=3) {
+    strip.setPixelColor(i + pixelQueue, Wheel((i + pixelCycle) % 255)); //  Update delay time  
+  }
+  strip.show();
+  for(int i=0; i < pixelNumber; i+=3) {
+    strip.setPixelColor(i + pixelQueue, strip.Color(0, 0, 0)); //  Update delay time  
+  }      
+  pixelQueue++;                           //  Advance current queue  
+  pixelCycle++;                           //  Advance current cycle
+  if(pixelQueue >= 3)
+    pixelQueue = 0;                       //  Loop
+  if(pixelCycle >= 256)
+    pixelCycle = 0;                       //  Loop
+}
 
-  // Blue for 1s
-  showColor(0, 0, 255);
-  delay(1000);
+uint8_t rgb_values[3];
 
-  // Off for 1s
-  showColor(0, 0, 0);
-  delay(1000);
+#include <ArduinoBLE.h>
+
+BLEService ledService("19B10000-E8F2-537E-4F6C-D104768A1214"); // BLE LED Service
+
+// BLE LED Switch Characteristic - custom 128-bit UUID, read and writable by central
+BLEByteCharacteristic switchCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Hello World!");
+
+    // custom services and characteristics can be added as well
+    // begin initialization
+    if (!BLE.begin())
+    {
+        Serial.println("starting BLE failed!");
+
+        while (1)
+            ;
+    }
+
+    Serial.print("Peripheral address: ");
+    Serial.println(BLE.address());
+
+    // set advertised local name and service UUID:
+    BLE.setLocalName("LED");
+    BLE.setAdvertisedService(ledService);
+
+    // add the characteristic to the service
+    ledService.addCharacteristic(switchCharacteristic);
+
+    // add service
+    BLE.addService(ledService);
+
+    // set the initial value for the characeristic:
+    switchCharacteristic.writeValue(0);
+
+    // start advertising
+    BLE.advertise();
+
+    strip.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+    strip.show();  // Turn OFF all pixels ASAP
+
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW);
+
+}
+
+void loop()
+{
+    BLEDevice central = BLE.central();
+
+    // if a central is connected to peripheral:
+    if (central)
+    {
+        Serial.print("Connected to central: ");
+        // print the central's MAC address:
+        Serial.println(central.address());
+
+        // while the central is still connected to peripheral:
+        while (central.connected())
+        {
+            // if the remote device wrote to the characteristic,
+            // use the value to control the LED:
+            if (switchCharacteristic.written())
+            {
+                switch (switchCharacteristic.value())
+                {
+                case 'a':
+                    colorWipe(strip.Color(255, 0, 0), 20); // Red
+                    break;
+                case 'b':
+                    colorWipe(strip.Color(0, 255, 0), 20); // Green
+                    break;
+                case 'c':
+                    colorWipe(strip.Color(0, 0, 255), 20); // Blue
+                    break;
+                case 'd':
+                    theaterChase(strip.Color(255, 0, 0), 20); // Red
+                    break;
+                case 'e':
+                    theaterChase(strip.Color(0, 255, 0), 20); // Green
+                    break;
+                case 'f':
+                    theaterChase(strip.Color(255, 0, 255), 20); // Cyan
+                    break;
+                case 'g':
+                    rainbow(10);
+                    break;
+                case 'h':
+                    theaterChaseRainbow(20);
+                    break;
+                }
+            }
+        }
+    }
 }
